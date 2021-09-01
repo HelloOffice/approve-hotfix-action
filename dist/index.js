6838b07691bc32b795e252a2f57108aa3a4082e0
Oscar Paramo <oscar@raise.work>
2021-09-01 16:43:06 -0500

    Hotfix/test2 (#3)
    * testing action
    
    * testing action again
    
    * fix action
    
    * fix action maybe
    
    * try it again
    
    * try something else
    
    * try another thing
    
    * print some values
    
    * fix action again
    
    * Fix typo
    
    * log some stuff
    
    * try it for real
    
    * try it for real this time
    
    * testing it :(
    
    * get the number
    
    * get the number again
    
    * get the number again again
    
    * Clean up code comments and add review approval comment.

--- dist/index.js
+++ dist/index.js
@@ -6334,54 +6334,19 @@ const main = async () => {
     const octokit = getOctokit(token);
     const number = context.payload.pull_request.number;
 
-    // console.info(github)
-    // console.info('full context number: ', context.payload.number);
-    // console.info('pull_request: ', context.payload.pull_request);
-    // console.info('...context: ', ...context);
-    // console.info('...context.repo: ', ...context.repo);
-    // console.info('number: ', number);
-    // core.info(github.context);
-    // console.info(octokit)
-    // core.info(sourceBranch);
-
     core.info('Approving Hotfix Pull Request... ⏱');
-
-    // const prList = await octokit.rest.pulls.list({
-    //     ...context.repo
-    // });
-
-    // console.info(prList);
-    // core.info(prList);
-
-    // prList.data.forEach((pr) => {
-    //     console.info('head: ', pr.head);
-    //     console.info('base: ', pr.base);
-    //     console.info('number: ', pr.number);
-    // })
 
     await octokit.rest.pulls.createReview({
         ...context.repo,
         pull_number: number,
-        event: 'APPROVE'
+        event: 'APPROVE',
+        body: '🚨⚠️ Hotfix PR approved, merge with caution. ⚠️🚨'
     });
 
     core.info('Hotfix Pull Request approved ✔');
 };
 
 main().catch((err) => core.setFailed(err.message));
-
-// try {
-//     // `who-to-greet` input defined in action metadata file
-//     const nameToGreet = core.getInput('who-to-greet');
-//     console.log(`Hello ${nameToGreet}!`);
-//     const time = (new Date()).toTimeString();
-//     core.setOutput("time", time);
-//     // Get the JSON webhook payload for the event that triggered the workflow
-//     const payload = JSON.stringify(github.context.payload, undefined, 2)
-//     console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//     core.setFailed(error.message);
-// }
 
 })();
 
